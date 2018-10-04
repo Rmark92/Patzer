c616279b9c8bf4ba6b0b31649eed8ddfab29c464
Ryan Mark <ryanmark2014@u.northwestern.edu>
2018-10-04 13:54:08 -0400

    condense stepMove function

--- position/movements/step_move.js
+++ position/movements/step_move.js
@@ -1,58 +1,19 @@
 const BitBoard = require('../../bitboards/bitboard');
 const GlobMasks = require('../../bitboards/masks/global.js');
 
-function north(positions) {
-  return positions.shiftLeft(8);
-}
-
-function south(positions) {
-  return positions.shiftRight(8);
-}
-
-function east(positions) {
-  return positions.shiftLeft(1).and(GlobMasks.EAST_OF_COL[0]);
-}
-
-function west(positions) {
-  return positions.shiftRight(1).and(GlobMasks.EAST_OF_COL[6]);
-}
-
-function excludeLeft(numCols) {
-  let res = GlobMasks.FULL_BOARD;
-  let idx = 0;
-  while (idx < numCols) {
-    res = res.xor(GlobMasks.COLS[idx]);
-    idx++;
-  }
-
-  return res;
-}
-
-function excludeRight(numCols) {
-  let res = GlobMasks.FULL_BOARD;
-  let idx = 7;
-  while ((7 - idx) < numCols) {
-    res = res.xor(GlobMasks.COLS[idx]);
-    idx--;
-  }
-
-  return res;
-}
-
 function stepMove(initial, noSo, eaWe) {
   let positions = initial;
 
   if (noSo > 0) {
     positions = positions.shiftLeft(noSo * 8);
-  } else {
+  } else if (noSo < 0){
     positions = positions.shiftRight(noSo * -8);
   }
 
   if (eaWe > 0) {
-    positions = positions.shiftLeft(eaWe).and(excludeLeft(eaWe));
-  } else {
-    positions = positions.shiftRight(-eaWe);
-    positions = positions.and(excludeRight(-eaWe));
+    positions = positions.shiftLeft(eaWe).and(GlobMasks.EAST_OF_COL[eaWe - 1]);
+  } else if (eaWe < 0){
+    positions = positions.shiftRight(-eaWe).and(GlobMasks.WEST_OF_COL[eaWe + 8]);
   }
 
   return positions;
