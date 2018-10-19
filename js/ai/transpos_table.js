001ba6b9fc0c87e4751a11a92d3606bd4b171ad9
Ryan Mark <ryanmark2014@u.northwestern.edu>
2018-10-19 15:40:32 -0400

    add transpos table class

---
+++ js/ai/transpos_table.js
@@ -0,0 +1,38 @@
+class TransposTable {
+  constructor() {
+    this.size = 10000;
+    this.table = new Array(this.size);
+  }
+
+  getEntry(key) {
+    const index = key % this.size;
+    if (this.table[index] && this.table[index].key === key) {
+      return this.table[index].key;
+    } else {
+      return null;
+    }
+  }
+
+  storeEntry(score, bestMove, alpha, beta, depth, key) {
+    const index = key % this.size;
+    this.table[index] = {
+      score,
+      bestMove,
+      type: this.determineScoreType(score, alpha, beta),
+      depth,
+      key
+    };
+  }
+
+  determineScoreType(score, alpha, beta) {
+    if (score <= alpha) {
+      return 'upperbound';
+    } else if (score >= beta) {
+      return 'lowerbound';
+    } else {
+      return 'exact';
+    }
+  }
+}
+
+module.exports = TransposTable;
