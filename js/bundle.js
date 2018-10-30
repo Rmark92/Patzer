282153ed0b42d9ec00e691ed05d6aa14becf9406
Ryan Mark <ryanmark2014@u.northwestern.edu>
2018-10-30 13:16:50 -0400

    add personal links

--- js/bundle.js
+++ js/bundle.js
@@ -60,7 +60,7 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 11);
+/******/ 	return __webpack_require__(__webpack_require__.s = 10);
 /******/ })
 /************************************************************************/
 /******/ ([
@@ -71,7 +71,7 @@
 
 
 var BitBoard = __webpack_require__(6);
-var Masks = __webpack_require__(15);
+var Masks = __webpack_require__(14);
 
 module.exports = {
   BitBoard: BitBoard,
@@ -91,14 +91,14 @@ function _defineProperty(obj, key, value) { if (key in obj) { Object.definePrope
 
 // const { WhitePawns, BlackPawns } = require('./pawns.js');
 var Constants = __webpack_require__(2);
-var Pawns = __webpack_require__(17);
-var Knight = __webpack_require__(18);
-var Bishop = __webpack_require__(19);
-var Rook = __webpack_require__(20);
-var Queen = __webpack_require__(21);
-var King = __webpack_require__(22);
-var PieceConv = __webpack_require__(23);
-var eachPieceType = __webpack_require__(24);
+var Pawns = __webpack_require__(16);
+var Knight = __webpack_require__(17);
+var Bishop = __webpack_require__(18);
+var Rook = __webpack_require__(19);
+var Queen = __webpack_require__(20);
+var King = __webpack_require__(21);
+var PieceConv = __webpack_require__(22);
+var eachPieceType = __webpack_require__(23);
 var Dirs = __webpack_require__(3);
 
 var PUtils = (_PUtils = {}, _defineProperty(_PUtils, Constants.Types.PAWNS, Pawns), _defineProperty(_PUtils, Constants.Types.KNIGHTS, Knight), _defineProperty(_PUtils, Constants.Types.BISHOPS, Bishop), _defineProperty(_PUtils, Constants.Types.ROOKS, Rook), _defineProperty(_PUtils, Constants.Types.QUEENS, Queen), _defineProperty(_PUtils, Constants.Types.KINGS, King), _PUtils);
@@ -315,7 +315,7 @@ var _createClass = function () { function defineProperties(target, props) { for 
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var Utils = __webpack_require__(14);
+var Utils = __webpack_require__(13);
 // A standard 8x8 chess board can be represented by a 64bit integer (bitboard),
 // in which a 1 means the position is occupied, a 0 means it's empty
 
@@ -633,7 +633,7 @@ var _createClass = function () { function defineProperties(target, props) { for 
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var _require = __webpack_require__(16),
+var _require = __webpack_require__(15),
     Types = _require.Types;
 
 var Move = function () {
@@ -740,8 +740,7 @@ function stepMove(initial, noSo, eaWe) {
 module.exports = stepMove;
 
 /***/ }),
-/* 9 */,
-/* 10 */
+/* 9 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -785,13 +784,13 @@ var Selectors = {
 module.exports = { ColsFiles: ColsFiles, FilesCols: FilesCols, RowsRanks: RowsRanks, RanksRows: RanksRows, Selectors: Selectors, GameStatus: GameStatus };
 
 /***/ }),
-/* 11 */
+/* 10 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
 
-var UI = __webpack_require__(12);
+var UI = __webpack_require__(11);
 
 $(document).ready(function () {
   var ui = new UI();
@@ -799,7 +798,7 @@ $(document).ready(function () {
 });
 
 /***/ }),
-/* 12 */
+/* 11 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -811,8 +810,8 @@ function _defineProperty(obj, key, value) { if (key in obj) { Object.definePrope
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var Position = __webpack_require__(13);
-var AI = __webpack_require__(27);
+var Position = __webpack_require__(12);
+var AI = __webpack_require__(26);
 
 var _require = __webpack_require__(1),
     PTypes = _require.PTypes,
@@ -822,9 +821,9 @@ var _require = __webpack_require__(1),
 var _require2 = __webpack_require__(7),
     MoveTypes = _require2.MoveTypes;
 
-var Util = __webpack_require__(32);
+var Util = __webpack_require__(31);
 
-var _require3 = __webpack_require__(10),
+var _require3 = __webpack_require__(9),
     ColsFiles = _require3.ColsFiles,
     FilesCols = _require3.FilesCols,
     RowsRanks = _require3.RowsRanks,
@@ -1305,7 +1304,7 @@ var UI = function () {
 module.exports = UI;
 
 /***/ }),
-/* 13 */
+/* 12 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -1329,13 +1328,13 @@ var _require3 = __webpack_require__(1),
     Colors = _require3.Colors,
     Dirs = _require3.Dirs;
 
-var _require4 = __webpack_require__(25),
+var _require4 = __webpack_require__(24),
     piecePosHashKeys = _require4.piecePosHashKeys,
     epPosHashKeys = _require4.epPosHashKeys,
     castleHashKeys = _require4.castleHashKeys,
     turnHashKeys = _require4.turnHashKeys;
 
-var _require5 = __webpack_require__(26),
+var _require5 = __webpack_require__(25),
     pieceSetsToArray = _require5.pieceSetsToArray,
     pieceSetsFromArray = _require5.pieceSetsFromArray;
 
@@ -2144,7 +2143,7 @@ var Position = function () {
 module.exports = Position;
 
 /***/ }),
-/* 14 */
+/* 13 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -2199,7 +2198,7 @@ module.exports = {
 };
 
 /***/ }),
-/* 15 */
+/* 14 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -2340,7 +2339,7 @@ module.exports = {
 };
 
 /***/ }),
-/* 16 */
+/* 15 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -2359,7 +2358,7 @@ var Types = typesArr.reduce(function (res, type, idx) {
 module.exports = { Types: Types };
 
 /***/ }),
-/* 17 */
+/* 16 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -2404,7 +2403,7 @@ var Pawns = {
 module.exports = Pawns;
 
 /***/ }),
-/* 18 */
+/* 17 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -2424,7 +2423,7 @@ var Knight = {
 module.exports = Knight;
 
 /***/ }),
-/* 19 */
+/* 18 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -2444,7 +2443,7 @@ var Bishop = {
 module.exports = Bishop;
 
 /***/ }),
-/* 20 */
+/* 19 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -2464,7 +2463,7 @@ var Rook = {
 module.exports = Rook;
 
 /***/ }),
-/* 21 */
+/* 20 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -2485,7 +2484,7 @@ var Queen = {
 module.exports = Queen;
 
 /***/ }),
-/* 22 */
+/* 21 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -2529,7 +2528,7 @@ var King = {
 module.exports = King;
 
 /***/ }),
-/* 23 */
+/* 22 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -2572,7 +2571,7 @@ function letterToType(letter) {
 module.exports = { pieceToLetter: pieceToLetter, letterToColor: letterToColor, letterToType: letterToType };
 
 /***/ }),
-/* 24 */
+/* 23 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -2591,7 +2590,7 @@ function eachPieceType(cb) {
 module.exports = eachPieceType;
 
 /***/ }),
-/* 25 */
+/* 24 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -2673,7 +2672,7 @@ var turnHashKeys = function () {
 module.exports = { piecePosHashKeys: piecePosHashKeys, epPosHashKeys: epPosHashKeys, castleHashKeys: castleHashKeys, turnHashKeys: turnHashKeys };
 
 /***/ }),
-/* 26 */
+/* 25 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -2765,7 +2764,7 @@ function pieceSetsFromArray() {
 module.exports = { pieceSetsToArray: pieceSetsToArray, pieceSetsFromArray: pieceSetsFromArray };
 
 /***/ }),
-/* 27 */
+/* 26 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -2775,7 +2774,7 @@ var _createClass = function () { function defineProperties(target, props) { for 
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var MoveSearch = __webpack_require__(28);
+var MoveSearch = __webpack_require__(27);
 
 var AI = function () {
   function AI() {
@@ -2807,7 +2806,7 @@ var AI = function () {
 module.exports = AI;
 
 /***/ }),
-/* 28 */
+/* 27 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -2824,12 +2823,12 @@ var _require = __webpack_require__(1),
     Dirs = _require.Dirs,
     eachPieceType = _require.eachPieceType;
 
-var _require2 = __webpack_require__(29),
+var _require2 = __webpack_require__(28),
     TransposTable = _require2.TransposTable,
     TABLE_SCORE_TYPES = _require2.TABLE_SCORE_TYPES;
 
-var KillerMoveList = __webpack_require__(30);
-var PerfMonitor = __webpack_require__(31);
+var KillerMoveList = __webpack_require__(29);
+var PerfMonitor = __webpack_require__(30);
 
 var EARLY_EXIT = 'early exit';
 
@@ -3083,7 +3082,7 @@ var MoveSearch = function () {
 module.exports = MoveSearch;
 
 /***/ }),
-/* 29 */
+/* 28 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -3141,7 +3140,7 @@ var TransposTable = function () {
 module.exports = { TransposTable: TransposTable, TABLE_SCORE_TYPES: SCORE_TYPES };
 
 /***/ }),
-/* 30 */
+/* 29 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -3192,7 +3191,7 @@ var KillerMoveList = function () {
 module.exports = KillerMoveList;
 
 /***/ }),
-/* 31 */
+/* 30 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -3278,7 +3277,7 @@ var PerfMonitor = function () {
 module.exports = PerfMonitor;
 
 /***/ }),
-/* 32 */
+/* 31 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -3290,7 +3289,7 @@ var _require = __webpack_require__(1),
     PieceConv = _require.PieceConv,
     Colors = _require.Colors;
 
-var _require2 = __webpack_require__(10),
+var _require2 = __webpack_require__(9),
     ColsFiles = _require2.ColsFiles,
     FilesCols = _require2.FilesCols,
     RowsRanks = _require2.RowsRanks,
